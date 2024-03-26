80acbb83fdacd6f6d5237256ad46a407000c7339
Andy Lau <andyhplau@gmail.com>
2024-03-25 18:35:08 -0700

    test with encoded db user password

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const dbConfigLocal = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+const encodedPassword = encodeURIComponent(process.env.DB_PASSWORD);
+const dbConfigLocal = `mysql://${process.env.DB_USER}:${encodedPassword}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
 
 var databaseConnectionString = dbConfigLocal;
 
